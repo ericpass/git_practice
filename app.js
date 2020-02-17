4f14d9a5cd77aee6850fb4212b255ab823abb321
Eric Pass <eric.b.pass@gmail.com>
2020-02-17 10:50:33 -0500

    no idea

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ console.log('Hello world');
 
 const definitelyNotAFunc = () => {
     console.log('Definitely not a function');
-<<<<<<< HEAD
-=======
 };
 
 const maybeAFunc = () => {
     console.log('Still nope');  
->>>>>>> newbranch
 }
