e7256eeee40fb235e8c04a940388a3adf93a50b5
Eric Pass <eric.b.pass@gmail.com>
2020-02-17 10:36:53 -0500

    Added new func

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ console.log('Hello world');
 
 const definitelyNotAFunc = () => {
     console.log('Definitely not a function');
-    
+};
+
+const maybeAFunc = () => {
+    console.log('Still nope');  
 }
