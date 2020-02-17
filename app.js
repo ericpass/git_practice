6a5139f39336638555bb35c4525dc9c3cf3b029a
Eric Pass <eric.b.pass@gmail.com>
2020-02-17 10:31:59 -0500

    Updating function

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 console.log('Hello world');
 
-<<<<<<< HEAD
-function test() {
-    console.log('Another test');  
-}
-=======
 const definitelyNotAFunc = () => {
     console.log('Definitely not a function');
-    
 }
->>>>>>> newbranch
